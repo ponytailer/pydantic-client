2bc014209e667fb503e70572027d5211a1820372
Ethon <huangxiaohen2738@gmail.com>
2025-11-26 19:27:02 +0800

    remove async client in __init__

--- pydantic_client/__init__.py
+++ pydantic_client/__init__.py
@@ -1,13 +1,10 @@
 from .decorators import delete, get, patch, post, put
 from .sync_client import RequestsWebClient
-from .async_client import AiohttpWebClient, HttpxWebClient
 from .base import BaseWebClient
 
 __all__ = [
     "BaseWebClient",
     "RequestsWebClient",
-    "AiohttpWebClient",
-    "HttpxWebClient",
     "get",
     "post",
     "put",
